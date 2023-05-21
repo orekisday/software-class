c25229e2f0bf82db5855ebf0c7701427aa8e8c93
orekisday <nurmukhammed.ernestov@alatoo.edu.kg>
2023-05-21 13:36:18 +0600

    prev changes

---
+++ Desktop/fatFreeTest2/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '02b4c0ed526c6d07cc98876f06637b83c8b155e5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '02b4c0ed526c6d07cc98876f06637b83c8b155e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'bcosca/fatfree-core' => array(
+            'pretty_version' => '3.8.1',
+            'version' => '3.8.1.0',
+            'reference' => 'f6ea45472f55a1ecabd4ced74cb56f0efd6af34e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bcosca/fatfree-core',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
